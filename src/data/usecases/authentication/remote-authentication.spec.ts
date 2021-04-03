cdec700b72abf384fbbf9f92d48af469a8a9c320
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2021-04-02 22:01:04 -0300

    refactor: change HttpPostClient to receive an object

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -6,6 +6,7 @@ type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
+
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
