c5d3bf77676100b7ba11e6e7d1254813a75cf61b
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2021-04-07 09:13:44 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import '@/presentation/styles/global.scss'
-import Login from '@/presentation/pages/login/login'
+import { Router } from '@/presentation/components'
 
 ReactDOM.render(
-    <Login/>,
+    <Router />,
     document.getElementById('main')
 )
---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Login } from '@/presentation/pages'
+
+const Router: React.FC = () => {
+  return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" >
+                    <Login />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+  )
+}
+
+export default Router
