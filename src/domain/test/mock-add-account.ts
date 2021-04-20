7eabdfbc142ec4b98e6805ce1fba0326618d5b0e
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2021-04-20 09:41:19 -0300

    feat: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,12 @@
+import faker from 'faker'
+import { AddAccountParams } from '../usecases'
+
+export const mockAddAccount = (): AddAccountParams => {
+  const password = faker.internet.password()
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    passwordConfirmation: password
+  }
+}
